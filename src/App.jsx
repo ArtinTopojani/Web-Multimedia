ac0bdc2dfdd95972328640bc4c1c855b346db960
Artin Topojani <artintopojani2002@gmail.com>
2024-06-06 01:23:30 +0200

    Pixi.js effect 1/2 added

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,24 @@ import Parallax from './components/parallax/Parallax';
 import Services from './components/services/Services';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
+import PixiCursor from './components/pixicursorfollower/PixiCursor';
 
 function App() {
   return (
     <>
+      <PixiCursor></PixiCursor>
+
       <section id='Homepage'>
         <Navbar></Navbar>
         <Hero />
       </section>
-
       <section id='Services'>
         <Parallax type='services' />
       </section>
       <section>
         <Services />
       </section>
-
       <Portfolio />
-
       <section id='Contact'>
         <Contact />
       </section>
---
+++ src/components/pixicursorfollower/PixiCursor.jsx
@@ -0,0 +1,64 @@
+// src/components/pixitest/PixiTestComponent.jsx
+import React, { useEffect, useState, useRef } from 'react';
+import { Stage, Sprite } from '@pixi/react';
+import cursorImg from '../../assets/crusorfollower.png';
+
+const PixiCursor = () => {
+  const [appSize, setAppSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const [spritePosition, setSpritePosition] = useState({
+    x: appSize.width / 2,
+    y: appSize.height / 2,
+  });
+  const spriteRef = useRef(null);
+
+  const handleResize = () => {
+    setAppSize({ width: window.innerWidth, height: window.innerHeight });
+  };
+
+  const handleMouseMove = (event) => {
+    setSpritePosition({ x: event.clientX, y: event.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        pointerEvents: 'none',
+        zIndex: 1000,
+      }}
+    >
+      <Stage
+        width={appSize.width}
+        height={appSize.height}
+        options={{ backgroundAlpha: 0, transparent: true }}
+      >
+        <Sprite
+          image={cursorImg}
+          anchor={0.5}
+          x={spritePosition.x}
+          y={spritePosition.y}
+          ref={spriteRef}
+        />
+      </Stage>
+    </div>
+  );
+};
+
+export default PixiCursor;
